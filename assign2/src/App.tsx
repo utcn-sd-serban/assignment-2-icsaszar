323b8e9925c37ed40c175ff8fc3f2760c3cd27bb
Istvan Csaszar <icsaszar@outlook.com>
2019-04-15 18:20:21 +0300

    Fixes header styling

--- assign2/src/App.tsx
+++ assign2/src/App.tsx
@@ -10,7 +10,6 @@ class App extends Component {
     return (
         <div>
           <Header/>
-          <hr/>
           <SmartFilterView/>
           <SmartNewPostView/>
           <SmartQuestionListView/>
--- assign2/src/view/Header.tsx
+++ assign2/src/view/Header.tsx
@@ -2,11 +2,10 @@ import React from 'react';
 import logo from "../resources/HeckOverflow.png";
 
 export const Header = () =>(
-    <div style={{display: "inline", whiteSpace: "nowrap"}}>
-        <img src={logo} style={{height: 100, whiteSpace: "nowrap"}}/>
-        <h1>
-            {/*https://i.redd.it/2k1ut81gpcl01.png*/}
-            Heck<b>Overflow</b>
-        </h1>
-    </div>);
+    <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+            <img src={logo} style={{height:50, background: "transparent"}} className="d-inline-block align-top" alt=""/>
+            <span style={{fontSize: 30}}> Heck<b>Overflow</b></span>
+        </a>
+    </nav>);
 
