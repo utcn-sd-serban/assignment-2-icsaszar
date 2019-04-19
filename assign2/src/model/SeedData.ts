08c886eac2ff15517bc1f48206f74babf9de2c13
Istvan Csaszar <icsaszar@outlook.com>
2019-04-19 13:19:14 +0300

    Adds addAction action to question store

--- assign2/src/model/SeedData.ts
+++ assign2/src/model/SeedData.ts
@@ -1,6 +1,7 @@
 import User from "./objects/User";
 import Question from "./objects/Question";
 import Tag from "./objects/Tag";
+import Answer from "./objects/Answer";
 
 export const users: User[] = [
     {
@@ -14,7 +15,22 @@ export const users: User[] = [
     {
         id: 3,
         name: "thedudehimself"
+    },
+    {
+        id: 4,
+        name: "opisanidiot"
+    },
+    {
+        id: 5,
+        name: "justfinishedcollege"
     }
+];
+
+const answers: Answer[] = [
+    new Answer("You do B", users[2], 1),
+    new Answer("Why would anybody want to do A?", users[3], 2),
+    new Answer("It's 2019, nobody uses A", users[4], 3),
+    new Answer("I have the same problem", users[1], 4)
 ];
 
 export const tags: Tag[] = [
@@ -31,11 +47,13 @@ export const questions: Question[] = [
         "Help my program keeps crashing",
         users[0],
         tags.slice(3, 4),
-        1),
+        1,
+        answers.slice(3,4)),
     new Question(
         "How do I A?",
         "Can someone please explain me how to do A?",
         users[1],
         tags.slice(0, 2),
-        2)
+        2,
+        answers.slice(0, 3))
 ];
--- assign2/src/model/question/actions.ts
+++ assign2/src/model/question/actions.ts
@@ -1,34 +1,20 @@
 import User from "../objects/User";
 import {
+    ADD_ANSWER_TO_QUESTION,
     ADD_TAG_TO_SELECTED_TAGS,
     CLEAR_NEW_POST_DATA,
     NEW_POST, NewPostField,
     QuestionActions,
-    SET_CURRENT_TAG, SET_NEW_POST_FIELD,
-    SET_NEW_POST_TEXT,
-    SET_NEW_POST_TITLE
+    SET_CURRENT_TAG, SET_NEW_POST_FIELD
 } from "./types";
 import Tag from "../objects/Tag";
+import Answer from "../objects/Answer";
 
 
 export function doNewPost(author: User): QuestionActions{
     return {
         type: NEW_POST,
         postAuthor: author
-    };
-}
-
-export function doSetNewTitle(newTitle: string): QuestionActions{
-    return {
-        type: SET_NEW_POST_TITLE,
-        newTitle: newTitle
-    };
-}
-
-export function doSetNewText(newText: string): QuestionActions{
-    return {
-        type: SET_NEW_POST_TEXT,
-        newText: newText
     };
 }
 
@@ -56,5 +42,13 @@ export function doAddTagToSelectedTags(): QuestionActions {
 export function doClearNewPostData(): QuestionActions {
     return {
         type: CLEAR_NEW_POST_DATA
+    };
+}
+
+export function doAddAnswer(questionId: number, newAnswer: Answer): QuestionActions{
+    return {
+        type: ADD_ANSWER_TO_QUESTION,
+        newAnswer: newAnswer,
+        targetQuestionId: questionId
     };
 }
