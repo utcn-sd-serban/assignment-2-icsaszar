00abced3e8bbfff5dfd5a1e5d2b68c0ac0260019
Istvan Csaszar <icsaszar@outlook.com>
2019-04-19 12:50:23 +0300

    Changes string to enum in question selector

--- assign2/src/model/question/selectors.ts
+++ assign2/src/model/question/selectors.ts
@@ -1,15 +1,16 @@
 import {AppState} from "../Model";
+import {QuestionFilter} from "../filter/types";
 
 export const getFilteredQuestions = (state: AppState) => {
     const {questions} = state.questionState;
     const {currentFilter, searchedTitle, searchedTag} = state.filterState;
 
     switch (currentFilter) {
-        case "ALL_POSTS":
+        case QuestionFilter.ALL_POSTS:
             return questions;
-        case "FILTER_BY_TITLE":
+        case QuestionFilter.FILTER_BY_TITLE:
             return questions.filter(q => q.title.toLowerCase().includes(searchedTitle.toLowerCase()));
-        case "FILTER_BY_TAG":
+        case QuestionFilter.FILTER_BY_TAG:
             return questions.filter(q => q.tags.includes(searchedTag));
         default:
             return questions;
