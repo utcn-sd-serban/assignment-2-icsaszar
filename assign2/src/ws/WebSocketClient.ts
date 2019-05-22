a1b13fbf4670981eed7ac7786724694cc79a8d75
Istvan Csaszar <icsaszar@outlook.com>
2019-05-22 10:58:22 +0300

    Adds New Answer event to WebSocket client

--- assign2/src/ws/WebSocketClient.ts
+++ assign2/src/ws/WebSocketClient.ts
@@ -3,12 +3,15 @@ import {ThunkDispatch} from "redux-thunk";
 import {AppState} from "../model/Model";
 import {Command} from "../model/command/types";
 import Question from "../model/objects/Question";
-import {doNewPost} from "../model/question/postlist/actions";
+import {doAddAnswer, doNewPost} from "../model/question/postlist/actions";
 import {Vote} from "../model/objects/Vote";
 import {doAddVote} from "../model/user/actions";
+import {fetchPost, fetchPosts} from "../model/question/postlist/asyncActions";
+import Answer from "../model/objects/Answer";
 
 const NEW_QUESTION_EVENT = "NEW_QUESTION";
 const NEW_VOTE_EVENT = "NEW_VOTE";
+const NEW_ANSWER_EVENT = "NEW_ANSWER";
 
 interface NewQuestionEvent {
     type: typeof NEW_QUESTION_EVENT;
@@ -20,16 +23,29 @@ interface NewVoteEvent {
     payload: Vote
 }
 
-type Event = NewQuestionEvent | NewVoteEvent
+interface NewAnswerEvent {
+    type: typeof NEW_ANSWER_EVENT
+    payload: {
+        questionId: number,
+        answer: Answer
+    }
+}
+
+type Event = NewQuestionEvent | NewVoteEvent | NewAnswerEvent
 
 export class WebSocketClient{
     private readonly client: Client;
     private readonly dispatch: ThunkDispatch<AppState, undefined, Command>;
     private static wsClient?: WebSocketClient = undefined;
 
-    static make(username: string, password: string, dispatch: ThunkDispatch<AppState, undefined, Command>){
-        if(this.wsClient === undefined)
-            this.wsClient = new WebSocketClient(username, password, dispatch)
+    static initialize(username: string, password: string, dispatch: ThunkDispatch<AppState, undefined, Command>){
+        if(this.wsClient !== undefined){
+            this.wsClient.client.deactivate();
+            delete this.wsClient;
+            this.wsClient = undefined
+        }
+
+        this.wsClient = new WebSocketClient(username, password, dispatch);
     }
 
     private constructor(username: string, password: string, dispatch: ThunkDispatch<AppState, undefined, Command>){
@@ -64,6 +80,13 @@ export class WebSocketClient{
                 case "NEW_VOTE":
                     let newVote = data.payload;
                     this.dispatch(doAddVote(newVote.postId, newVote.direction));
+                    // I know this is not nice, but i dont't want to have a
+                    // separate DTO on the backend (just for this case) that includes the score
+                    // (this is just reusing VoteDTO) and
+                    // a separate reducer that just updates the score
+                    // A simpler solution could be to increment/decrement the score here (in the frontend)
+                    // and hope for the best, but that doesn't feel right
+                    this.dispatch(fetchPosts(true));
                     break;
                 case "NEW_QUESTION":
                     let newQuestion = data.payload;
@@ -73,6 +96,13 @@ export class WebSocketClient{
                     };
                     this.dispatch(doNewPost(newQuestion));
                     break;
+                case "NEW_ANSWER":
+                    let {answer, questionId} = data.payload;
+                    answer = {
+                        ...answer,
+                        posted: new Date(answer.posted)
+                    };
+                    this.dispatch(doAddAnswer(answer, questionId))
             }
         })
     }
